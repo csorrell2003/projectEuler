399e2d01fe8f74cdcd214d61ce7bd6ba2facd0c7
csorrell2003 <csorrell2003@yahoo.com>
2014-10-07 09:15:22 -0400

    initial commit

---
+++ problem-3.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+function factor($n){
+$i=1;
+while ($i *$i <= $n)
+{ 
+
+	//I found that using the %modulus can not handle numbers larger than 2^32. bcmod() provided the solution
+	if (bcmod($n, $i) == 0)
+	{
+		$n = $n / $i;
+		echo $i . "<br>";
+	}
+	//We can skip even iterations knowing that they won't be prime
+	$i = $i +2; 
+}
+return $n;
+}
+
+$n = 600851475143;
+settype($n, "float");
+echo factor($n);
+echo "<br>";
+?>
